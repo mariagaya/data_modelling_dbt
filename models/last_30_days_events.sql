23bc6cc3912d4bc9cba24cd6e2f5809d332d28f4
mariagaya <62390742+mariagaya@users.noreply.github.com>
2022-04-10 20:13:34 +0200

    Pivot last_30_days_events

--- models/last_30_days_events.sql
+++ models/last_30_days_events.sql
@@ -1,3 +1,21 @@
-select *
-from gitbook-analytics.itw_test.user_event
-where event_datetime
+{% macro dateadd(datepart, interval, from_date_or_timestamp) %}
+  {{ adapter_macro('dbt_utils.dateadd', datepart, interval, from_date_or_timestamp) }}
+{% endmacro %}
+
+with last_30_days_events as (
+
+    select * 
+    from {{ ref('stg_event') }} 
+    where dbt_utils.dateadd(day, 30, event_datetime),
+  
+  
+pivot_events as (
+  select
+   user_id,
+   {{ dbt_utils.pivot('event_name', dbt_utils.get_column_values(ref('last_30_days_events'), count('event_name'), max(event_datetime))) }}
+from {{ ref('last_30_days_events') }} 
+group by user_id
+
+)
+
+select * from pivot_events
