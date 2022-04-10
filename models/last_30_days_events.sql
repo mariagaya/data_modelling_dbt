75972473b5d0da2ff58543b66e9ba83b40e272e3
mariagaya <62390742+mariagaya@users.noreply.github.com>
2022-04-10 20:28:37 +0200

    Fix macro

--- models/last_30_days_events.sql
+++ models/last_30_days_events.sql
@@ -2,17 +2,44 @@
   {{ adapter_macro('dbt_utils.dateadd', datepart, interval, from_date_or_timestamp) }}
 {% endmacro %}
 
+{% macro pivot_event_name(column,
+               values,
+               alias=True,
+               agg='count',
+               cmp='=',
+               prefix='',
+               suffix='_count',
+               then_value=1,
+               else_value=0,
+               quote_identifiers=True,
+               distinct=False) %}
+    {{ return(adapter.dispatch('pivot', 'dbt_utils')(column, values, alias, agg, cmp, prefix, suffix, then_value, else_value, quote_identifiers, distinct)) }}
+{% endmacro %}
+
+-- {% macro pivot_event_name(column,
+--                values,
+--                alias=True,
+--                agg='max',
+--                cmp='=',
+--                prefix='',
+--                suffix='_last_date',
+--                then_value=1,
+--                else_value=0,
+--                quote_identifiers=True,
+--                distinct=False) %}
+--     {{ return(adapter.dispatch('pivot', 'dbt_utils')(column, values, alias, agg, cmp, prefix, suffix, then_value, else_value, quote_identifiers, distinct)) }}
+-- {% endmacro %}
+
 with last_30_days_events as (
 
     select * 
     from {{ ref('stg_event') }} 
-    where dbt_utils.dateadd(day, 30, event_datetime),
-  
+    where dateadd(day, 30, event_datetime),
   
 pivot_events as (
   select
    user_id,
-   {{ dbt_utils.pivot('event_name', dbt_utils.get_column_values(ref('last_30_days_events'), count('event_name'), max(event_datetime))) }}
+   {{ pivot_event_name('event_name', dbt_utils.get_column_values(ref('last_30_days_events'), 'event_name')) }}
 from {{ ref('last_30_days_events') }} 
 group by user_id
 
