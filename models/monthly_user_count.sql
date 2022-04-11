1387cf50ae1effbdec25da6eda5cad3ac8deeb72
mariagaya <62390742+mariagaya@users.noreply.github.com>
2022-04-11 11:45:34 +0200

    Cummulative count

--- models/monthly_user_count.sql
+++ models/monthly_user_count.sql
@@ -1,41 +1,56 @@
-with user_reg_and_unreg as (
 
-    (select month(user_signuped_time) as activity_month
-          company_id,
-          count(distinct user_id) as registered_users
-    from {{ ref('stg_users') }}
-    where user_leaved_time is not null
-    group by month(user_signuped_time), company_id)
-  
-    union
-  
-    (select month(user_signuped_time) as activity_month
-          company_id,
-          count(distinct user_id) as unregistered_users
-    from {{ ref('stg_users') }}
-    where user_leaved_time is null
-    group by month(user_signuped_time), company_id)
-),
-  
-members_count as (
-      select activity_month,
-              company_id,
-              registered_users - unregistered_users as members_count
-      from {{ ref('user_reg_and_unreg') }} 
-      group by activity_month, company_id
-      
+
+with cumulative_users_count as (
+                select
+                    ur.year_month,
+                    ur.company_id,
+                    max(ur.user_registered) AS members_count
+                from (
+                    select
+                        reg_unreg.year_month,
+                        reg_unreg.company_id,
+                        SUM(reg_unreg.user_count) over (order by reg_unreg.year_month) as user_registered
+                    from
+                        (
+                            (
+                                select
+                                    date_format(convert_tz(from_unixtime(created_at), '+00:00', '+02:00'), '%x-M%m') as year_month,
+                                    company_id,
+                                    count(distinct user_id) as user_count
+                                from {{ ref('stg_user') }}
+                                group by date_format(convert_tz(from_unixtime(created_at), '+00:00', '+02:00'), '%x-M%m'), ladenid
+                            )
+
+                            union all
+
+                            (
+                                select
+                                    date_format(convert_tz(from_unixtime(user_leaved_time), '+00:00', '+02:00'), '%x-M%m') as year_month,
+                                    company_id,
+                                    count(distinct user_id) as user_count
+                                from {{ ref('stg_user') }}
+                                where user_leaved_time IS NOT NULL
+                                group by date_format(convert_tz(from_unixtime(user_leaved_time), '+00:00', '+02:00'), '%x-M%m'), ladenid
+
+                            )
+                        ) AS reg_unreg
+                 ) AS ur
+            group by ur.year_month, ur.company_id
+            order by ur.company_id, ur.year_month;
 ),
 
 company_tier as (
-      select activity_month,
-       company_id,
-       case 
-          when members_count < 10 then 'C'
-          when members_count < 50 then 'B'
-          else 'A'
-       end as company_tier
-    from {{ ref('members_count') }} 
-    group by activity_month, company_id
+      select 
+           year_month,
+           company_id,
+           case 
+              when members_count < 10 then 'C'
+              when members_count < 50 then 'B'
+              else 'A'
+           end as company_tier,
+           members_count
+       from {{ ref('cumulative_users_count') }} 
+       group by year_month, company_id
 )
 
 select * from company_tier
