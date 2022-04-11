953b6de4d72f290011a7d8ab5ba28370c8cd3a55
mariagaya <62390742+mariagaya@users.noreply.github.com>
2022-04-11 12:39:19 +0200

    Fix pivot table

--- models/last_30_days_events.sql
+++ models/last_30_days_events.sql
@@ -2,47 +2,31 @@
   {{ adapter_macro('dbt_utils.dateadd', datepart, interval, from_date_or_timestamp) }}
 {% endmacro %}
 
-{% macro pivot_event_name(column,
-               values,
-               alias=True,
-               agg='count',
-               cmp='=',
-               prefix='',
-               suffix='_count',
-               then_value=1,
-               else_value=0,
-               quote_identifiers=True,
-               distinct=False) %}
-    {{ return(adapter.dispatch('pivot', 'dbt_utils')(column, values, alias, agg, cmp, prefix, suffix, then_value, else_value, quote_identifiers, distinct)) }}
-{% endmacro %}
-
--- {% macro pivot_event_name(column,
---                values,
---                alias=True,
---                agg='max',
---                cmp='=',
---                prefix='',
---                suffix='_last_date',
---                then_value=1,
---                else_value=0,
---                quote_identifiers=True,
---                distinct=False) %}
---     {{ return(adapter.dispatch('pivot', 'dbt_utils')(column, values, alias, agg, cmp, prefix, suffix, then_value, else_value, quote_identifiers, distinct)) }}
--- {% endmacro %}
-
 with last_30_days_events as (
-
-    select * 
-    from {{ ref('stg_event') }} 
-    where dateadd(day, 30, event_datetime),
+        select * 
+        from {{ ref('stg_event') }} 
+        where dateadd(day, 30, event_datetime),
   
-pivot_events as (
-  select
-   user_id,
-   {{ pivot_event_name('event_name', dbt_utils.get_column_values(ref('last_30_days_events'), 'event_name')) }}
-from {{ ref('last_30_days_events') }} 
-group by user_id
-
+pivot_event_name as (
+        select
+            user_id,
+            {{ dbt_utils.pivot('event_name',dbt_utils.get_column_values(ref('last_30_days_events'), 'event_name'),
+                agg='count',
+                suffix='_count') }}
+        from {{ ref('last_30_days_events') }} 
+        group by user_id
+)
+  
+pivot_event_date as (
+        select
+            user_id,
+            {{ dbt_utils.pivot('event_name',dbt_utils.get_column_values(ref('last_30_days_events'), 'event_datetime'),
+                agg='max',
+                suffix='_last_date') }}
+        from {{ ref('last_30_days_events') }} 
+        group by user_id
 )
 
-select * from pivot_events
+select *
+  from pivot_event_name en
+  left join pivot_event_date ed on en.user_id = ed.user_id
