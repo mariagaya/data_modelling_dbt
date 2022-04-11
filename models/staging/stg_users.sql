2c2d6ad6eab95bca3e2fde8189e2384c0916d41d
mariagaya <62390742+mariagaya@users.noreply.github.com>
2022-04-11 10:09:29 +0200

    Add user_leaved_time to last_entry

--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -6,10 +6,10 @@ with source as (
 
 first_entry as (
 
-    select user_id 
-        , company_id
-        , user_signuped_time
-        , MIN(_extracted_at) as first_entry_date
+    select user_id,
+            company_id,
+            user_signuped_time,
+            MIN(_extracted_at) as first_entry_date
     from source
     group by user_id;
 
@@ -17,17 +17,18 @@ first_entry as (
 
 last_entry as (
 
-    select user_id 
-        , company_id
-        , MAX(_extracted_at) as last_entry_date
+    select user_id,
+            company_id,
+            user_leaved_time,
+            MAX(_extracted_at) as last_entry_date
     from source
     group by user_id;
 
 )
 
 select fe.user_id,
-    fe.company_id,
-    fe.user_signuped_time,
-    le.user_leaved_time
+        fe.company_id,
+        fe.user_signuped_time,
+        le.user_leaved_time
 from first_entry fe
 left join last_entry le on fe.user_id = le.user_id
